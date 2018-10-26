7af67f2851374ebeebbcc320252f13844fca881a
Gary Dalton <gary@ggis.biz>
2018-10-25 20:08:38 -0500

    Some basic constants in main.hpp

---
+++ pros/include/main.hpp
@@ -0,0 +1,26 @@
+/**
+ * \file main.hpp
+ *
+ * Contains common definitions, constants, and header files used throughout.
+ *
+**/
+
+
+// Flag dimensions and signatures
+const double top_flag = 46.6;
+const double mid_flag = 32.4;
+const double bot_flag = 18.3;
+const double h_flag = 6.0;
+const double w_flag = 9.9;
+const double h_color = 5.5;
+const double w_color = 5.25;
+const double w_green = 1.75;
+// Cap dimnesions
+const double w_cap = 9.2;
+const double h_cap = 4.6;
+const double mass_cap = 335;
+// Ball and post dimensions
+const double dia_ball = 3.0;
+const double mass_ball = 55;
+const double h_short_post = 23.0;
+const double h_tall_post = 34.0;
